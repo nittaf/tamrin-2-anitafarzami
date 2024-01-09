c7093246478be6a80540ad96839e2e9ed508cae6
Anita Farzami <anita.farzami@Anitas-MacBook-Air.local>
2024-01-09 21:08:39 +0330

    counter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+class Counter extends Component {
+  state = {
+    number: 0,
+  };
 
+  handleNumberChange1 = () =>
+  {
+    this.setState({
+      number: this.state.number +1
+    })
+  }
+
+  handleNumberChange2 = () =>
+  {
+    this.setState({
+      number: this.state.number -1
+    })
+  }
+
+  handleNumberChange3 = () =>
+  {
+    this.setState({
+      number: this.state.number +5
+    })
+  }
+
+  handleNumberChange4 = () =>
+  {
+    this.setState({
+      number: this.state.number -5
+    })
+  }
+  render() {
+    return <div>
+    <div id='box' className='box' >
+      <button onClick={this.handleNumberChange1}>
+        +1
+      </button>
+      <button onClick={this.handleNumberChange2}>
+        -1
+      </button>   
+      <div>
+        {this.state.number}
+      </div>
+      <button onClick={this.handleNumberChange3}>
+        +5
+      </button>
+      <button onClick={this.handleNumberChange4}>
+        -5
+      </button>
+      
+    </div>
+  </div>;
+  }
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <Counter></Counter>
       </header>
     </div>
   );
